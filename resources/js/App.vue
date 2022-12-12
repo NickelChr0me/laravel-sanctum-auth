13deafee558216adb365d26d7cc59775d86fd29b
NickelChr0me <matthias.codvelle@le-campus-numerique.fr>
2022-12-12 11:38:19 +0100

    add daisyui

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,20 @@
 <template>
-    <div>
-        <h1 class="mt-5 text-4xl text-center hover:uppercase">
-            Hello world!
-        </h1>
+    <div class="text-4xl flex flex-col items-center">
+
+        <div class="navbar bg-base-100 shadow-xl decoration-white">
+            <a class="btn btn-primary normal-case text-xl" href="https://daisyui.com/" target="_blank">daisyUI</a>
+        </div>
+
+        <div class="my-5 card w-96 bg-base-100 shadow-xl">
+            <div class="card-body items-center">
+                <h2 class="card-title">Hello world!</h2>
+                <div class="card-actions justify-end">
+                    <a href="https://github.com/NickelChr0me/laravel-sanctum-auth/" target="_blank">
+                        <button class="btn btn-primary" >GitHub of project</button>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,13 +22,11 @@
 export default {
     name: "App.vue",
     mounted() {
-        console.log("Coucou de vue");
+        console.log("App.vue mounted !");
     }
 }
 </script>
 
 <style scoped>
-h1 {
-    color: blueviolet;
-}
+
 </style>
